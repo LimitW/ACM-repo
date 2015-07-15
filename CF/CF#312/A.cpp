c3bdd4b2785e0d4561345c913bd344fb092f7af6
LimitW <hbdgwb13@gmail.com>
2015-07-15 12:55:51 +0800

    ....silly

--- CF/CF#312/A.cpp
+++ CF/CF#312/A.cpp
@@ -23,31 +23,37 @@ using namespace std;
 const int INF = 0x3f3f3f3f;
 
 struct aa{
-	int v,x;
-	bool operator < (aa b){
-		return x < b.x;
-	}
+    int v, x;
+    bool operator < (aa b) const{
+        return x < b.x;
+    }
 }a[110];
 
 int main(){
-	int n; cin >> n;
-	for(int i = 0; i < n; ++i){
-		cin >> a[i].x >> a[i].v;
-	}
-	sort(a,a+n);
-	int tmp = -1;
-	for(int i = 0; i < n; ++i){
-		if(a[i] > 0) tmp = i;
-	}
-	int ans = 0;
-	if(tmp == 0) ans = a[0].v;
-	else if(tmp == -1) ans = a[n-1].v;
-	else{
-		for(int i = tmp-1,j=tmp+1; i >= 0 && j < n; --i,++j){
-			ans += a[i].v;
-			ans += a[j].v;
-		}
-	}
-	cout << ans << '\n';
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i){
+        cin >> a[i].x >> a[i].v;
+    }
+    sort(a, a + n);
+    int tmp = -1;
+    for (int i = 0; i < n; ++i){
+        if (a[i].x > 0){
+            tmp = i;
+            break;
+        }
+    }
+    int ans = 0;
+    if (tmp == 0) ans = a[0].v;
+    else if (tmp == -1) ans = a[n - 1].v;
+    else{
+        int i, j;
+        for (i = tmp - 1, j = tmp; i >= 0 && j < n; --i, ++j){
+            ans += a[i].v;
+            ans += a[j].v;
+        }
+        if (j < n) ans += a[j].v;
+        else if (i >= 0) ans += a[i].v;
+    }
+    cout << ans << '\n';
     return 0;
 }
